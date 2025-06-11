e5297f1a8e20601ab5be5eb5bbd16732f6ae9607
Youssef Hasnaoui <hasneouiyoussef@gmail.com>
2025-06-11 14:49:56 +0100

    Initial Commit

--- CoreMark_Csolution/tmp/coremark.Debug+F4_DISC/toolchain.cmake
+++
@@ -1,5 +0,0 @@
-# toolchain.cmake
-
-set(REGISTERED_TOOLCHAIN_ROOT "/home/youssef/Embedded/arm-gnu-14.2.1/bin")
-set(REGISTERED_TOOLCHAIN_VERSION "14.2.1")
-include("${CMSIS_COMPILER_ROOT}/GCC.10.3.1.cmake")
